ae9151f493eeeedcb87f578ae4f0277a4fd8d28f
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-11-07 17:18:53 +0100

    added:celsius to fahrenheit and fahrenheit to celsius

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-let fah = 0
-let celsius = 0
-function celsius_to_fah(celsius2: number): number {
-    
+fah = 0
+celsius = 0
+def celsius_to_fah(celsius2: number):
+    global fah
     fah = celsius2 * 9 / 5 + 32
     fah = Math.round(fah * 100) / 100
     return fah
-}
-
-function fah_to_celsius(fah2: number): number {
-    
+def fah_to_celsius(fah2: number):
+    global celsius
     celsius = (fah2 - 32) * 5 / 9
     celsius = Math.round(celsius * 100) / 100
     return celsius
-}
-
