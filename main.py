cbb07f364cb56570eaba5ef186fd798fd4fdf34c
DiegoCaroFrechilla <146827294+DiegoCaroFrechilla@users.noreply.github.com>
2024-11-11 15:05:30 +0100

    feature: logic

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-fah = 0
-celsius = 0
+def menu():
+    global opcio_usuari
+    opcio_usuari = game.ask_for_number("1: Celsius to Fahrenheit, 2: Fahrenheit to Celsius", 1)
+    return opcio_usuari
 def celsius_to_fah(celsius2: number):
-    global fah
-    fah = celsius2 * 9 / 5 + 32
-    fah = Math.round(fah * 100) / 100
-    return fah
+    global fah22
+    fah22 = celsius2 * 9 / 5 + 32
+    fah22 = Math.round(fah22 * 100) / 100
+    return fah22
+def salutacio(opcio: number):
+    if opcio == 1:
+        value = game.ask_for_number("Introduzca los Celsius", 5)
+        fah = celsius_to_fah(value)
+        game.show_long_text(fah, DialogLayout.TOP)
+    elif opcio == 2:
+        value = game.ask_for_number("Introduzca los Fahrenheit", 5)
+        celsius = fah_to_celsius(value)
+        game.show_long_text(celsius, DialogLayout.TOP)
+    else:
+        game.show_long_text("Opció no vàlida", DialogLayout.BOTTOM)
+    salutacio(menu())
 def fah_to_celsius(fah2: number):
-    global celsius
-    celsius = (fah2 - 32) * 5 / 9
-    celsius = Math.round(celsius * 100) / 100
-    return celsius
+    global celsius22
+    celsius22 = (fah2 - 32) * 5 / 9
+    celsius22 = Math.round(celsius22 * 100) / 100
+    return celsius22
+celsius22 = 0
+fah22 = 0
+opcio_usuari = 0
+# MAIN DE LA APP
+salutacio(menu())
--- main.ts
+++ main.ts
@@ -1,16 +1,44 @@
-let fah = 0
-let celsius = 0
+function menu(): number {
+    
+    opcio_usuari = game.askForNumber("1: Celsius to Fahrenheit, 2: Fahrenheit to Celsius", 1)
+    return opcio_usuari
+}
+
 function celsius_to_fah(celsius2: number): number {
     
-    fah = celsius2 * 9 / 5 + 32
-    fah = Math.round(fah * 100) / 100
-    return fah
+    fah22 = celsius2 * 9 / 5 + 32
+    fah22 = Math.round(fah22 * 100) / 100
+    return fah22
+}
+
+function salutacio(opcio: number) {
+    let value: number;
+    let fah: number;
+    let celsius: number;
+    if (opcio == 1) {
+        value = game.askForNumber("Introduzca los Celsius", 5)
+        fah = celsius_to_fah(value)
+        game.showLongText(fah, DialogLayout.Top)
+    } else if (opcio == 2) {
+        value = game.askForNumber("Introduzca los Fahrenheit", 5)
+        celsius = fah_to_celsius(value)
+        game.showLongText(celsius, DialogLayout.Top)
+    } else {
+        game.showLongText("Opció no vàlida", DialogLayout.Bottom)
+    }
+    
+    salutacio(menu())
 }
 
 function fah_to_celsius(fah2: number): number {
     
-    celsius = (fah2 - 32) * 5 / 9
-    celsius = Math.round(celsius * 100) / 100
-    return celsius
+    celsius22 = (fah2 - 32) * 5 / 9
+    celsius22 = Math.round(celsius22 * 100) / 100
+    return celsius22
 }
 
+let celsius22 = 0
+let fah22 = 0
+let opcio_usuari = 0
+//  MAIN DE LA APP
+salutacio(menu())
